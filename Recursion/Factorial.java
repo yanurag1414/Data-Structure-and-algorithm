5a481526a41f53262a94dae60558f29249ec8eca
yanurag1414 <121807496+yanurag1414@users.noreply.github.com>
2023-02-05 18:41:45 +0530

    factorial

---
+++ Recursion/Factorial.java
@@ -0,0 +1,13 @@
+package Recursion;
+
+public class Factorial {
+    public static void main(String[] args) {
+        System.out.println(fact(5));
+    }
+    static int fact(int n){
+        if(n==0){
+            return 1;
+        }
+        return n*fact(n-1);
+    }
+}
