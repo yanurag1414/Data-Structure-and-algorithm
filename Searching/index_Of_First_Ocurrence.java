7a17af162d5fb07d85cb1ea6523644773b0a5978
Anurag Yadav <yanurag1414@gmail.com>
2023-04-16 10:57:10 +0530

    Adding last ocurrence code

--- Searching/index_Of_First_Ocurrence.java
+++ Searching/index_Of_First_Ocurrence.java
@@ -3,8 +3,8 @@ package Searching;
 public class index_Of_First_Ocurrence {
     public static void main(String[] args) {
         int[] arr = {1,2,3,3,3,4,5};
-        System.out.println(firstIndex(arr,4));
-
+        System.out.println(firstIndex(arr,3));
+        System.out.println(lastIndex(arr,3));
     }
     static int firstIndex(int[] nums,int target){
         int start=0;
@@ -21,6 +21,31 @@ public class index_Of_First_Ocurrence {
                     return mid;
                 } else {
                     end = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+
+
+
+    //last ocurrence
+    static int lastIndex(int[] arr,int target){
+        int n = arr.length;
+        int s = 0;
+        int e = n-1;
+        while (s<=e){
+            int mid = s+(e+s)%2;
+            if(target>arr[mid]){
+                s = mid+1;
+            } else if (target<arr[mid]) {
+                e = mid-1;
+            }else {
+                if (mid == n - 1 || arr[mid] != arr[mid+1] ) {
+                    return mid;
+                } else {
+                    s = mid + 1;
                 }
             }
         }
