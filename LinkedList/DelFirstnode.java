86559f4c78052669dca8492fbc122fbf47a20ab7
yanurag1414 <yanurag1414@gmail.com>
2023-06-08 00:23:01 +0530

    delete first node in doubly linked list

--- LinkedList/DelFirstnode.java
+++ LinkedList/DelFirstnode.java
@@ -1,2 +1,33 @@
-package LinkedList;public class DelFirstnode {
+package LinkedList;
+
+public class DelFirstnode {
+    public static void main(String[] args) {
+        Nodedouble head=new Nodedouble(10);
+        Nodedouble temp1=new Nodedouble(20);
+        Nodedouble temp2=new Nodedouble(30);
+        head.next=temp1;
+        temp1.prev=head;
+        temp1.next=temp2;
+        temp2.prev=temp1;
+        head = delFirst(head);
+        Nodedouble curr = head;
+        while (curr!=null){
+
+            System.out.println(curr.data+" ");
+            curr = curr.next;
+        }
+
+    }
+    static Nodedouble delFirst(Nodedouble head){
+        if(head == null){
+            return null;
+        }
+        if(head.next == null){
+            return null;
+        }else{
+            head = head.next;
+            head.prev = null;
+            return head;
+        }
+    }
 }
