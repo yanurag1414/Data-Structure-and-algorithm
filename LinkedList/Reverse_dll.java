c7d1c9fcc0b072c6483ecdc98ff7758b155fc620
yanurag1414 <yanurag1414@gmail.com>
2023-06-08 00:26:13 +0530

    search element in linked list

--- LinkedList/Reverse_dll.java
+++ LinkedList/Reverse_dll.java
@@ -1,2 +1,38 @@
-package LinkedList;public class Reverse_dll {
+package LinkedList;
+
+
+public class Reverse_dll {
+    public static void main(String[] args) {
+        Nodedouble head=new Nodedouble(10);
+        Nodedouble temp1=new Nodedouble(20);
+        Nodedouble temp2=new Nodedouble(30);
+        head.next=temp1;
+        temp1.prev=head;
+        temp1.next=temp2;
+        temp2.prev=temp1;
+        head=reverse(head);
+
+        Nodedouble curr = head;
+        while (curr!=null){
+            System.out.println(curr.data+" ");
+            curr = curr.next;
+        }
+
+
+    }
+    static Nodedouble reverse(Nodedouble head){
+        if(head==null || head.next==null){
+            return head;
+        }
+        Nodedouble prev = null;
+        Nodedouble curr = head;
+        while (curr!=null){
+            prev = curr.prev;
+            curr.prev = curr.next;
+            curr.next = prev;
+            curr = curr.prev;
+
+        }
+        return prev.prev;
+    }
 }
--- LinkedList/Search.java
+++ LinkedList/Search.java
@@ -1,2 +1,48 @@
-package LinkedList;public class Search {
+package LinkedList;
+
+public class Search {
+    public static void main(String[] args) {
+        Nodedouble head = new Nodedouble(20);
+        head.next = new Nodedouble(30);
+        head.next.next = new Nodedouble(10);
+        System.out.println(searchEle(head,10));
+        System.out.println(rSearch(head,10));
+
+    }
+    static int searchEle(Nodedouble head, int x){
+        int pos = 1;
+        Nodedouble curr = head;
+        while (curr!=null){
+            if(curr.data==x){
+                return pos;
+            }else {
+                pos++;
+                curr = curr.next;
+            }
+        }
+        return -1;
+    }
+
+
+
+
+
+
+    //recursive method
+    static int rSearch(Nodedouble head, int x){
+        if(head==null){
+            return -1;
+        }
+        if(head.data==x){
+            return 1;
+        }else {
+            int res = rSearch(head.next,x);
+            if(res==-1){
+                return -1;
+            }else {
+                return res+1;
+            }
+        }
+
+    }
 }
