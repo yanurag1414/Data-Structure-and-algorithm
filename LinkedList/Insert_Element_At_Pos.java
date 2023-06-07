f1fff4f6077e6ae63a6cf155e2113191f93f319d
yanurag1414 <yanurag1414@gmail.com>
2023-06-08 00:23:47 +0530

    insert element in single linked list at given position

--- LinkedList/Insert_Element_At_Pos.java
+++ LinkedList/Insert_Element_At_Pos.java
@@ -1,2 +1,42 @@
-package LinkedList;public class Insert_Element_At_Pos {
+package LinkedList;
+
+
+public class Insert_Element_At_Pos {
+    public static void main(String[] args) {
+        Nodedouble head = new Nodedouble(20);
+        head.next = new Nodedouble(30);
+        head.next.next = new Nodedouble(40);
+        head.next.next.next = new Nodedouble(50);
+        head = insetAtpos(1,10,head);
+        Nodedouble curr = head;
+        while (curr!=null) {
+            System.out.println(curr.data + " ");
+            curr = curr.next;
+        }
+
+
+    }
+
+
+
+
+
+    static Nodedouble insetAtpos(int pos, int data, Nodedouble head){
+        Nodedouble temp = new Nodedouble(data);
+        if(pos==1){
+            temp.next = head;
+            return temp;
+        }
+        Nodedouble curr = head;
+        for (int i = 1; i < pos-1 && curr!=null; i++) {
+            curr = curr.next;
+            if(curr==null){
+                return head;
+            }
+        }
+
+        temp.next = curr.next;
+        curr.next = temp;
+        return head;
+    }
 }
